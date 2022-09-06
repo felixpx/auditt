661c5d9bd1df4e8ffb396adafd68be287ae6d9ee
Felix <dexdaprotocol@gmail.com>
2022-09-06 16:15:48 +0200

    Eth Online Init
    Initialization of AUDITT for ETH ONLINE / ETH GLOBAL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MoralisProvider } from "react-moralis";
+import { MoralisDappProvider } from "../src/MoralisDappProvider/MoralisDappProvider";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// const APP_ID = process.env.NEXT_APP_MORALIS_APP_ID;
+const APP_ID = "PwE9zZL74dyQiIdDo5qFP1uUIkq6xTVgiP9ul7Fb";
+// const SERVER_URL = process.env.NEXT_APP_MORALIS_SERVER_URL;
+const SERVER_URL = "https://fscxsw3halcr.usemoralis.com:2053/server";
+
+function Auditt({ Component, pageProps }: AppProps) {
+  const isServerInfo = APP_ID && SERVER_URL ? true : false;
+  //validate
+  if (!APP_ID || !SERVER_URL) throw new Error("Missing Moralis Credentials");
+  if (isServerInfo)
+    return (
+      <MoralisProvider
+        appId={APP_ID}
+        serverUrl={SERVER_URL}
+        initializeOnMount={true}
+      >
+        <MoralisDappProvider>
+          <Component {...pageProps} />
+        </MoralisDappProvider>
+      </MoralisProvider>
+    );
 }
 
-export default MyApp
+export default Auditt;
